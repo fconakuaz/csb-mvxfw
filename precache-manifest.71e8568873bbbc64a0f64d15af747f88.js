dd062c27ebe9c5910cddf37a34f5f5045a090546
Code Sandbox <hello@codesandbox.io>
2021-08-09 03:00:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.71e8568873bbbc64a0f64d15af747f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a478c19fa48394735733119037e3ebc7",
+    "url": "https://fconakuaz.github.io/csb-mvxfw/index.html"
+  },
+  {
+    "revision": "abbe7681378bb6997e3a",
+    "url": "https://fconakuaz.github.io/csb-mvxfw/static/css/main.586df6b9.chunk.css"
+  },
+  {
+    "revision": "0721e42d272373a55bb5",
+    "url": "https://fconakuaz.github.io/csb-mvxfw/static/js/2.b96823ce.chunk.js"
+  },
+  {
+    "revision": "abbe7681378bb6997e3a",
+    "url": "https://fconakuaz.github.io/csb-mvxfw/static/js/main.55c7d210.chunk.js"
+  },
+  {
+    "revision": "df614d91db8a53e8f797",
+    "url": "https://fconakuaz.github.io/csb-mvxfw/static/js/runtime~main.a8dbee89.js"
+  },
+  {
+    "revision": "a2817827e6175bf2a1ae5d604908ae50",
+    "url": "https://fconakuaz.github.io/csb-mvxfw/static/media/nobooks.a2817827.jpg"
+  }
+]);
